8f38cfb6991ae63af068a000842a2c5a9232741a
SethPalmer3 <elderlord30@gmail.com>
2023-10-19 10:52:14 -0700

    Changed the User Profile model
    The User Profile no longer has a profile field. It now also has a json field as to hopefully add the partition data into later

--- LBM/users/models.py
+++ LBM/users/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import json
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    detail_json = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.user.username
 
--- LBM/users/views.py
+++ LBM/users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import HttpResponse, render, redirect, reverse
 from django.contrib.auth import authenticate, login
+
+from .models import UserProfile
 from .forms import SignUpForm
 
 # Create your views here.
+# Login page (initial page)
 def user_login(request):
     if request.method == "POST":
         username = request.POST.get('username')
@@ -17,15 +20,20 @@ def user_login(request):
     return render(request, 'login.html')
 
 def user_signup(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
+    if request.method == "POST": # If the user has inputted data
+        form = SignUpForm(request.POST) # Parse signup form information
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
+            user.set_password(form.cleaned_data['password']) # Setting password
             user.save()
-            login(request, user)
-            return redirect('login')
-    else:
+
+            profile = UserProfile() # Custom user information
+            profile.user = user
+            profile.save()
+
+            login(request, user) # Persistant login of user
+            return redirect(reverse('users:home')) # Redirects to their new home screen
+    else: # First time going to page
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
