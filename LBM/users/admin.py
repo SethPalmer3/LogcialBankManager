aed16b9ad3c5619cbe644e2e45adac35b1db33f3
SethPalmer3 <elderlord30@gmail.com>
2023-10-31 14:04:19 -0700

    Added some styling for messages

--- LBM/users/admin.py
+++ LBM/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ExternalWebApp, UserProfile, Partition
+from .models import ExternalWebApp, UserProfile
+from partitions.models import Partition
 
 # Register your models here.
 admin.site.register(UserProfile)
--- LBM/users/urls.py
+++ LBM/users/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('home/', views.user_home, name='home'),
-    # path('partition/<uuid:partition_id>/', views.user_partition_view, name='partition'),
-    # path('partitionedit/<uuid:partition_id>/', views.user_partition_edit, name='edit_partition'),
-    # path('addpartition/', views.add_partition, name='add_partition'),
-    # path('removepartition/<uuid:partition_id>/', views.remove_partiton, name='remove_partition'),
 ] 
