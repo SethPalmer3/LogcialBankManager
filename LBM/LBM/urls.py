9b10bd0cdac0e8f2429fdb61db5b407ade9e54ae
SethPalmer3 <elderlord30@gmail.com>
2023-11-27 13:10:35 -0800

    Changed static files url

--- LBM/LBM/urls.py
+++ LBM/LBM/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include("users.urls")),
     path('', include("partitions.urls")),
     path('', include("logins.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
